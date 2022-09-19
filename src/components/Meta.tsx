182f1228169f3a39ad72c82f393cb75c38393fa4
Sergey-la <bogdanetsserg@yandex.ru>
2022-09-19 19:34:47 +0300

    удалён скелетон

---
+++ src/components/Meta.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const Meta: FC = () => {
+	return <div>Meta</div>;
+};
+
+export default Meta;
--- src/pages/home/Pizzaz/Pizzaz.tsx
+++ src/pages/home/Pizzaz/Pizzaz.tsx
@@ -1,26 +1,18 @@
 import React, { FC } from 'react';
 
 import Pizza from './Pizza';
-import Skeleton from './Skeleton';
 import { PizzaList } from './pizza.interface';
 
 const Pizzaz: FC<PizzaList> = ({ pizzaz }) => {
-	const [isLoaded, setIsloaded] = React.useState(true);
-
-	React.useEffect(() => {
-		setIsloaded(false);
-	}, []);
-
-	const render = () => {
-		if (isLoaded) {
-			return [...new Array(6)].map((_, i) => <Skeleton key={i} />);
-		} else if (pizzaz?.length && !isLoaded) {
-			return pizzaz?.map((item) => <Pizza key={item.id} pizza={item} />);
-		}
-		return <div>Упс, пицц нет</div>;
-	};
-
-	return <React.Fragment>{render()}</React.Fragment>;
+	return (
+		<React.Fragment>
+			{pizzaz?.length ? (
+				pizzaz.map((item) => <Pizza key={item.id} pizza={item} />)
+			) : (
+				<div></div>
+			)}
+		</React.Fragment>
+	);
 };
 
 export default Pizzaz;
