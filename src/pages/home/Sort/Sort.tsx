51ef4fda415d0f2621b31fe5b3e7ddb27ba83909
Sergey-la <bogdanetsserg@yandex.ru>
2022-09-19 10:59:22 +0300

    компонент сортировки

--- src/pages/home/Sort/Sort.tsx
+++ src/pages/home/Sort/Sort.tsx
@@ -1,8 +1,17 @@
-import { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import styles from './Sort.module.scss';
 
 const Sort: FC = () => {
+	const [isActive, setIsActive] = React.useState(false);
+	const [sort, setSort] = useState('популярности');
+	const data = ['популярности', 'цене', 'алфавиту'];
+
+	const changeSort = (sortParam: string) => {
+		setIsActive(!isActive);
+		setSort(sortParam);
+	};
+
 	return (
 		<div className={styles.sort}>
 			<div className={styles.sort__label}>
@@ -19,13 +28,25 @@ const Sort: FC = () => {
 					/>
 				</svg>
 				<b>Сортировка по:</b>
-				<span>популярности</span>
+				<span onClick={() => setIsActive(!isActive)}>{sort}</span>
 			</div>
-			<div className={styles.sort__popup}>
+			<div
+				className={
+					isActive
+						? `${styles.sort__popup} ${styles['sort__popup--active']}`
+						: `${styles.sort__popup}`
+				}
+			>
 				<ul>
-					<li className={styles.active}>популярности</li>
-					<li>цене</li>
-					<li>алфавиту</li>
+					{data.map((item) => (
+						<li
+							key={item}
+							className={item === sort ? `${styles.active}` : ''}
+							onClick={() => changeSort(item)}
+						>
+							{item}
+						</li>
+					))}
 				</ul>
 			</div>
 		</div>
